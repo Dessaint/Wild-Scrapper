5a407ecfbebcc48a6529793005767b6c71af9dca
Cédric Oloa <cedric.oloa@laposte.net>
2016-07-20 23:22:54 +0200

    Query Builder

--- src/MeetupBundle/Controller/PageController.php
+++ src/MeetupBundle/Controller/PageController.php
@@ -4,13 +4,41 @@ namespace MeetupBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use UserBundle\Entity\User;
+use Symfony\Component\HttpFoundation\Response;
 
 class PageController extends Controller
 {
     public function showAction()
     {
-        // replace this example code with whatever you need
-        return $this->render('MeetupBundle:Default:accueil.html.twig');
+        $repository = $this
+            ->getDoctrine()
+            ->getManager()
+            ->getRepository('MeetupBundle:GroupesPHP')
+            ;
+
+            // $membresPHPParis = $repository->MembersByCity('PHP', 'paris');
+            // $membresJavaScriptParis = $repository->MembersByCity('JavaScript', 'paris');
+            // $membresRubyParis = $repository->MembersByCity('Ruby', 'paris');
+            // $membresIOSParis = $repository->MembersByCity('IOS', 'paris');
+            //
+            // $membresPHPParis = $membresPHPParis[0];
+            // $membresJavaScriptParis = $membresJavaScriptParis[0];
+            // $membresRubyParis = $membresRubyParis[0];
+            // $membresIOSParis = $membresIOSParis[0];
+            $graph = $repository->Graph();
+
+            $response = new Response();
+            $response->setContent(json_encode($graph));
+            $response->headers->set('Content-Type', 'application/json');
+            var_dump($response); exit;
+
+        return $this->render('MeetupBundle:Default:accueil.html.twig'
+        // array(
+        //     'membresPHPParis'       =>$membresPHPParis,
+        //     'membresJavaScriptParis'=>$membresJavaScriptParis,
+        //     'membresRubyParis'      =>$membresRubyParis,
+        //     'membresIOSParis'    =>$membresIOSParis
+        // )
+    );
     }
 }
-
